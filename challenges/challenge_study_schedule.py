ee4224c028d48786ab2723b616b36537b214ef06
itaji-create <itaji.1999@gmail.com>
2022-10-20 13:00:01 -0300

    feat: cria algorítimo para calcular pessoas disponiveis em determinados espaçoes de tempo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if isinstance(target_time, int):
+        qtd = 0
+        for p in permanence_period:
+            if isinstance(p[1], int) and isinstance(p[0], int):
+                if p[0] <= target_time <= p[1]:
+                    qtd += 1
+            else:
+                return None
+        return qtd
+    else:
+        return None
