2db59340f903aa8a99179cafc5a10efb583d60a1
itaji-create <itaji.1999@gmail.com>
2022-10-20 14:03:01 -0300

    refactor: reduz complexidade da função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    if isinstance(target_time, int):
-        qtd = 0
+    qtd = 0
+    try:
         for p in permanence_period:
-            if isinstance(p[1], int) and isinstance(p[0], int):
-                if p[0] <= target_time <= p[1]:
-                    qtd += 1
-            else:
-                return None
+            if int(target_time) in range(p[0], p[1]+1):
+                qtd += 1
         return qtd
-    else:
+    except TypeError:
         return None
