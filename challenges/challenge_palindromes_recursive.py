ba5f1bcf53fca428d48e1339fd289ee90713a8f2
itaji-create <itaji.1999@gmail.com>
2022-10-20 18:49:27 -0300

    feat: Essa função irá determinar se uma palavra é um palíndromo ou não

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,17 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    # try:
+    if len(word) > 100:
+        raise RecursionError
+    if word == '':
+        return False
+    while low_index < high_index:
+        if word[low_index] != word[high_index]:
+            print("not recursive")
+            return False
+        low_index += 1
+        high_index -= 1
+
+    return True
+
+
+is_palindrome_recursive('', 0, len('')-1)
