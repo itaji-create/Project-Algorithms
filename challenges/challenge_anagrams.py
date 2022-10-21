901414baff01cf6b543194e82357b59a7bf755be
itaji-create <itaji.1999@gmail.com>
2022-10-21 10:28:32 -0300

    feat: Essas funções ordenam e comparam strings para verificar se são anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,24 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    first_string = order_string(first_string)
+    second_string = order_string(second_string)
+
+    if first_string != second_string:
+        # print("not anagram")
+        return False
+    # print("anagram")
+    return True
+
+
+def order_string(string):
+    _array = [i for i in string.lower()]
+
+    _length = len(_array)
+    for order in range(_length - 1):
+        for item in range(0, _length - 1 - order):
+            if _array[item] > _array[item + 1]:
+                current_letter = _array[item]
+                _array[item] = _array[item + 1]
+                _array[item + 1] = current_letter
+    # print(_array)
+
+    return _array
