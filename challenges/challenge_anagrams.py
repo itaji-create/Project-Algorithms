6003689f6d4a635f9411c3cf087cba7000927cfb
itaji-create <itaji.1999@gmail.com>
2022-10-21 22:07:37 -0300

    refactor: muda complexidade para O(n log n)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,24 +1,32 @@
 def is_anagram(first_string, second_string):
-    first_string = order_string(first_string)
-    second_string = order_string(second_string)
+    first_array = [i for i in first_string.lower()]
+    second_array = [i for i in second_string.lower()]
 
-    if first_string != second_string:
+    quick_sort(first_array, 0, len(first_array) - 1)
+    quick_sort(second_array, 0, len(second_array) - 1)
+    if first_array != second_array:
         # print("not anagram")
         return False
     # print("anagram")
     return True
 
 
-def order_string(string):
-    _array = [i for i in string.lower()]
+def quick_sort(numbers, start, end):
+    if start < end:
+        p = partition(numbers, start, end)
+        quick_sort(numbers, start, p - 1)
+        quick_sort(numbers, p + 1, end)
 
-    _length = len(_array)
-    for order in range(_length - 1):
-        for item in range(0, _length - 1 - order):
-            if _array[item] > _array[item + 1]:
-                current_letter = _array[item]
-                _array[item] = _array[item + 1]
-                _array[item + 1] = current_letter
-    # print(_array)
 
-    return _array
+def partition(numbers, start, end):
+    pivot = numbers[end]
+    delimiter = start - 1
+
+    for index in range(start, end):
+        if numbers[index] <= pivot:
+            delimiter = delimiter + 1
+            numbers[index], numbers[delimiter] = numbers[delimiter], numbers[index]
+
+    numbers[delimiter + 1], numbers[end] = numbers[end], numbers[delimiter + 1]
+
+    return delimiter + 1
