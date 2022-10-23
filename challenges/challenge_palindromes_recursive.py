99b153c5b345e2239d12c6a8dbd9c02162ca335a
itaji-create <itaji.1999@gmail.com>
2022-10-23 14:34:42 -0300

    refactor: reduz complexidade da função

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,13 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if len(word) > 100:
-        raise RecursionError
     if word == '':
         return False
-    while low_index < high_index:
-        if word[low_index] != word[high_index]:
-            print("not recursive")
-            return False
-        low_index += 1
-        high_index -= 1
+    if low_index >= high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
-    return True
+
+# is_palindrome_recursive('banana', 0, 5)
